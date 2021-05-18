ecd5e91e3743628733d80a483786465c2b69928b
github username <github email address>
2021-05-17 23:56:42 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -5,7 +5,7 @@ class Comment extends React.Component {
   render() {
     return (
       <div className="comment">
-        { this.props.commentText }
+        {this.props.commentText}
       </div>
     )
   }
